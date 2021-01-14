2fd73dcc493f9538a8a153cadb7b3f3d80428cf3
taeyoonlee0118 <62052230+taeyoonlee0118@users.noreply.github.com>
2021-01-14 16:21:47 +0900

    Add files via upload

---
+++ app_db.py
@@ -0,0 +1,19 @@
+sectors = {
+    'watchlist' : ['DAL', 'PLTR', 'SNOW', 'CDNS', 'AAPL', 'MA'],
+    'platform': ['AMZN', 'MS', 'GOOG', 'FB'],
+    'healthcare': ['PFE', 'MRNA', 'AZN', 'TDOC', 'LLY', 'JNJ', 'MRK', 'NVAX', 'GILD', 'CRSP'],
+    'e-commerce' : ['SE', 'MELI', 'JMIA','SHOP', 'ABNB','CHWY', 'Z', 'FTCH'],
+    'hardware': ['AAPL', 'CSCO', 'NVDA', 'TSM', 'INTC', 'AMD', 'QCOM', 'MU', 'ASML', 'AMAT', 'TER'],
+    'software': ['NOW', 'CRM', 'SAP', 'ZM', 'WORK', 'TTD', 'ORCL', 'VMW', 'SNOW', 'DOCU', 'U', 'CDNS', 'SNPS', 'CRWD', 'PLTR'],
+    'media': ['DIS', 'NFLX', 'ROKU', 'SPOT', 'SNAP', 'TWTR', 'PINS', 'ATVI'],
+    'autos': ['TSLA', 'GM', 'F', 'NIO', 'LI', 'UBER', 'LYFT'],
+    'finance': ['V', 'MA', 'PYPL', 'SQ', 'JPM', 'MS', 'C', 'GS', 'BLK'],
+    're-energy': ['BE', 'PLUG', 'ENPH', 'FSLR', 'SEDG', 'NEE', 'ARRY','VWDRY'],
+    'chemicals': ['XOM', 'CVX', 'MMM', 'DD', 'DOW', 'SLB'],
+    'consumers': ['WMT', 'COST', 'PG', 'KO', 'NKE', 'SBUX', 'EL'],
+    'industrials': ['LMT', 'BA', 'CAT', 'DAL', 'UAL', 'AAL', 'HON', 'ROK', 'ETN', 'CARR', 'UPS'],
+    'delivery': ['DASH', 'GRUB']
+}
+
+macros = {'fred': [],
+          }
