cce6ebf777a4227edc1dab0e6dcb9b3801da7317
taeyoonlee0118 <62052230+taeyoonlee0118@users.noreply.github.com>
2021-01-19 15:10:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ def load_data(stock_list, start_date):
     data_list = [yf.download(stock, start_date)['Close'] for stock in stock_list]
     data = pd.concat(data_list, axis=1)
     data.columns = [code for code in stock_list]
-    return data
+    long_name = [yf.Ticker(ticker).info['longName'] for ticker in data.columns]
+
+    return data, long_name
 
 
 # sidebar construct
@@ -29,16 +31,17 @@ macro_list = st.sidebar.multiselect('Select Indicators', macros[macro_choice], m
 st.sidebar.text('copywright by Taeyoon Lee')
 
 
-df = load_data(sectors[sector_choice], start_date)
+df, company_name = load_data(sectors[sector_choice], start_date)
 
 n = 1
-for stock in stock_list:
+for count, stock in enumerate(stock_list):
     day0_price = df[stock][-1]
     day1_price = df[stock][-2]
     note_up = f'price: {round(day0_price,2)} up by {(day0_price/day1_price -1)*100:.2f} %'
     note_down = f'price: {round(day0_price, 2)}  down by {(day0_price / day1_price - 1) * 100:.2f} %'
     if n == 1:
         col1.title(stock)
+        col1.write(company_name[count])
         if day0_price >= day1_price:
             col1.write(note_up)
         else:
@@ -47,6 +50,7 @@ for stock in stock_list:
         n = 2
     elif n == 2:
         col2.title(stock)
+        col2.write(company_name[count])
         if day0_price >= day1_price:
             col2.write(note_up)
         else:
